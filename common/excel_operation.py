76748d81582cb40bbaf7617216e8be40dc97faea
zhangyan <17701302629@163.com>
2021-11-30 06:19:23 +0800

    添加excel操作合并

--- common/excel_operation.py
+++ common/excel_operation.py
@@ -17,7 +17,51 @@ class ExcelOperation(object):
 
     # 读取excel文件方法
     def read_excel(self):
-        pass
+        # 新建总的空字典，存储每张sheet表的所有测试用例
+        caseDictTol = dict()
+        # 获取所有sheet表名列表['登录','总体态势',...]
+        sheets = self.wb.sheetnames
+        print("所有sheet表名：", sheets)
+        # # 接口测试用例每张sheet表的格式应一致，因此获取其中某个表单对象的总列数及对应字段
+        # cols = self.wb[sheets[0]].max_column
+        # print("最大列数：", cols)
+        # 遍历所有sheet表
+        for sheetname in sheets:
+            # 获取当前sheet表总行数
+            sheet = self.wb[sheetname]
+            rows = sheet.max_row
+            # 新建每张sheet表空列表，用于存放所有测试用例
+            caseList = list()
+            # 遍历excel某sheet表中每行数据
+            for i in range(2, rows + 1):
+                # 新建空字典，存储具体数据
+                caseDict = dict()
+                try:
+                    # 读取数据，追加到字典中
+                    caseDict['id'] = sheet.cell(i, 1).value
+                    caseDict['c_name'] = sheet.cell(i, 2).value
+                    caseDict['test_module'] = sheet.cell(i, 3).value
+                    caseDict['project_id'] = sheet.cell(i, 4).value
+                    caseDict['api_name'] = sheet.cell(i, 5).value
+                    caseDict['api_path'] = sheet.cell(i, 6).value
+                    caseDict['request_type'] = sheet.cell(i, 7).value
+                    caseDict['headers'] = sheet.cell(i, 8).value
+                    caseDict['params_type'] = sheet.cell(i, 9).value
+                    caseDict['params'] = sheet.cell(i, 10).value
+                    caseDict['sql'] = sheet.cell(i, 11).value
+                    caseDict['expectation'] = sheet.cell(i, 12).value
+                    caseDict['is_run'] = sheet.cell(i, 13).value
+                    caseDict['create_time'] = sheet.cell(i, 14).value
+                    caseDict['update_time'] = sheet.cell(i, 15).value
+                    caseDict['user_id'] = sheet.cell(i, 15).value
+                    caseDict['response'] = sheet.cell(i, 15).value
+                    # 将每行测试用例数据，追加到该sheet表的总用例列表中
+                    caseList.append(caseDict)
+                except Exception as e:
+                    print(e)
+            # 将字典追加到总字典中
+            caseDictTol[sheetname] = caseList
+        print("最终读取excel结果为：", caseDictTol)
 
     # 写入excel文件方法，传入写入sheet表，行与列（列表形式），数值
     def write_excel(self, sheet_name, x_y, content):
@@ -35,4 +79,5 @@ class ExcelOperation(object):
 
 if __name__ == '__main__':
     filename = "5GClient_testcase.xlsx"
-    ExcelOperation(filename).write_excel("登录", [2, 13], "cccccccc")
+    # ExcelOperation(filename).write_excel("登录", [2, 13], "cwwwwc")
+    ExcelOperation(filename).read_excel()
